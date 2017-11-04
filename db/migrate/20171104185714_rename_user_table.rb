168d2bd0a4443de536c42c1da5806efbb72d355c
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-11-04 14:10:55 -0600

    click event

---
+++ db/migrate/20171104185714_rename_user_table.rb
@@ -0,0 +1,5 @@
+class RenameUserTable < ActiveRecord::Migration[5.1]
+  def change
+  	rename_table :user, :users
+  end
+end
---
+++ public/javascript/main.js
@@ -0,0 +1,11 @@
+${'#sign_in'}.on('click' function(
+	${'#sign_in'}.text('logout');
+	${'#create_account'}.text(''); 
+
+	);
+
+${'#create_account_button'}.on('click' function(
+	${'#create_account'}.text('logout');
+	${'#sign_in'}.text('');
+
+	);
